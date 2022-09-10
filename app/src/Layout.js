4a8d675ac40e7899af2622ca64d3fa2f6902c769
Samuel Palacios <samuelpl0888@gmail.com>
2022-09-10 02:56:20 -0400

    Add metadata and apply lazy loading on components, still need to apply it to images

--- app/src/Layout.js
+++ app/src/Layout.js
@@ -1,26 +1,42 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import injectContext from "./store/appContext";
 import Home from "./pages/Home";
-import Specialists from "./pages/Specialists";
-import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import NavbarNew from "./components/NewNavbar";
-import SuperAdmin from "./pages/Dashboard/SuperAdmin";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import RegisterForm from "./components/RegisterForm";
-import Login from "./components/Login";
-import Profile from "./pages/Dashboard/Profile";
-import Doctores from "./pages/Dashboard/Doctores";
-import Pacientes from "./pages/Dashboard/Pacientes";
-import Appointment from "./components/Appointment";
-import FormEditService from "./components/Modal/Forms/FormEditService";
-import FormEditClient from "./components/Modal/Forms/FormEditClient";
-import FormEditAppointment from "./components/Modal/Forms/FormEditAppointment";
-import FormEditSpecialist from "./components/Modal/Forms/FormEditSpecialist";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
-import AppointmentFromDoctor from "./components/AppointmentFromDoctor";
+// import Specialists from "./pages/Specialists";
+const Specialists = lazy(() => import("./pages/Specialists"));
+// import Contact from "./pages/Contact";
+const Contact = lazy(() => import("./pages/Contact"));
+// import SuperAdmin from "./pages/Dashboard/SuperAdmin";
+const SuperAdmin = lazy(() => import("./pages/Dashboard/SuperAdmin"));
+// import Dashboard from "./pages/Dashboard/Dashboard";
+const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
+// import RegisterForm from "./components/RegisterForm";
+const RegisterForm = lazy(() => import("./components/RegisterForm"));
+// import Login from "./components/Login";
+const Login = lazy(() => import("./components/Login"));
+// import Profile from "./pages/Dashboard/Profile";
+const Profile = lazy(() => import("./pages/Dashboard/Profile"));
+// import Doctores from "./pages/Dashboard/Doctores";
+const Doctores = lazy(() => import("./pages/Dashboard/Doctores"));
+// import Pacientes from "./pages/Dashboard/Pacientes";
+const Pacientes = lazy(() => import("./pages/Dashboard/Pacientes"));
+// import Appointment from "./components/Appointment";
+const Appointment = lazy(() => import("./components/Appointment"));
+// import FormEditService from "./components/Modal/Forms/FormEditService";
+const FormEditService = lazy(() => import("./components/Modal/Forms/FormEditService"));
+// import FormEditClient from "./components/Modal/Forms/FormEditClient";
+const FormEditClient = lazy(() => import("./components/Modal/Forms/FormEditClient"));
+// import FormEditAppointment from "./components/Modal/Forms/FormEditAppointment";
+const FormEditAppointment = lazy(() => import("./components/Modal/Forms/FormEditAppointment"));
+// import FormEditSpecialist from "./components/Modal/Forms/FormEditSpecialist";
+const FormEditSpecialist = lazy(() => import("./components/Modal/Forms/FormEditSpecialist"));
+// import AppointmentFromDoctor from "./components/AppointmentFromDoctor";
+const AppointmentFromDoctor = lazy(() => import("./components/AppointmentFromDoctor"));
 
 const Layout = () => {
   const FooterCheck = () => {
@@ -40,36 +56,38 @@ const Layout = () => {
     }
   };
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <NavbarNew />
-      <Routes>
-        {/* PAGES */}
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/specialists" element={<Specialists />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* DASHBOARD  */}
-        <Route path="/admin" element={<SuperAdmin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/doctores" element={<Doctores />} />
-        <Route path="/pacientes" element={<Pacientes />} />
-        {/* AUTH COMPONENTS */}
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/login" element={<Login />} />
-        {/* WIZARD COMPONENTS */}
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="/appointmentdoctor" element={<AppointmentFromDoctor />} />
-        {/* EDIT ROUTES */}
-        <Route path="/edit/service/:id" element={<FormEditService />} />
-        <Route path="/edit/client/:id" element={<FormEditClient />} />
-        <Route path="/edit/specialist/:id" element={<FormEditSpecialist />} />
-        <Route path="/edit/appointment/:id" element={<FormEditAppointment />} />
-      </Routes>
-      <ToastContainer />
-      <FooterCheck />
-    </BrowserRouter>
+    <Suspense fallback={<h1>Cargando...</h1>}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <NavbarNew />
+        <Routes>
+          {/* PAGES */}
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/specialists" element={<Specialists />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* DASHBOARD  */}
+          <Route path="/admin" element={<SuperAdmin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/doctores" element={<Doctores />} />
+          <Route path="/pacientes" element={<Pacientes />} />
+          {/* AUTH COMPONENTS */}
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<Login />} />
+          {/* WIZARD COMPONENTS */}
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/appointmentdoctor" element={<AppointmentFromDoctor />} />
+          {/* EDIT ROUTES */}
+          <Route path="/edit/service/:id" element={<FormEditService />} />
+          <Route path="/edit/client/:id" element={<FormEditClient />} />
+          <Route path="/edit/specialist/:id" element={<FormEditSpecialist />} />
+          <Route path="/edit/appointment/:id" element={<FormEditAppointment />} />
+        </Routes>
+        <ToastContainer />
+        <FooterCheck />
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
